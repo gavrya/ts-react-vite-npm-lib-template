01ca6698b53ef70aa184186d86d306bc25c07007
Oleksandr <gavrya@gmail.com>
2023-09-23 21:44:19 +0300

    chore: semantic-release config update

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -20,7 +20,7 @@ module.exports = {
       '@semantic-release/git',
       {
         assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]',
+        message: 'chore(release): v${nextRelease.version} [skip ci]',
       },
     ],
     [
